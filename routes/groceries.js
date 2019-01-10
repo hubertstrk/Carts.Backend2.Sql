ecd290eb7f8302183b8f21e27360f789906c6349
Hubert Stärk <hubert.staerk@gmail.com>
2019-01-10 23:24:21 +0100

    :construction: change table names

--- routes/groceries.js
+++ routes/groceries.js
@@ -1,21 +1,21 @@
 const db = require('../js/db')
 
 exports.all = function (req, res) {
-  db.execute('SELECT * FROM Grocery')
+  db.execute('SELECT * FROM groceries')
   .then((result) => {
     res.send(result) 
   })
 }
 
 exports.delete = function (req, res) {
-  db.execute(`DELETE FROM Grocery WHERE Id = ${req.params.id}`)
+  db.execute(`DELETE FROM groceries WHERE Id = ${req.params.id}`)
   .then((result) => {
     res.send(result)
   })
 }
 
 exports.add = function (req, res) {
-  db.execute(`INSERT INTO Grocery (Name) VALUES ('${req.params.name}')`)
+  db.execute(`INSERT INTO groceries (Name) VALUES ('${req.params.name}')`)
   .then((result) => {
     res.send(result) 
   })
--- routes/matter.js
+++ routes/matter.js
@@ -1,21 +1,21 @@
 const db = require('../js/db')
 
 exports.all = function (req, res) {
-  db.execute('SELECT * FROM Matter')
+  db.execute('SELECT * FROM matters')
   .then((result) => {
     res.send(result) 
   })
 }
 
 exports.delete = function (req, res) {
-  db.execute(`DELETE FROM Matter WHERE Id = ${req.params.id}`)
+  db.execute(`DELETE FROM matters WHERE Id = ${req.params.id}`)
   .then((result) => {
     res.send(result)
   })
 }
 
 exports.add = function (req, res) {
-  db.execute(`INSERT INTO Matter (Name) VALUES ('${req.params.name}')`)
+  db.execute(`INSERT INTO matters (Name) VALUES ('${req.params.name}')`)
   .then((result) => {
     res.send(result) 
   })
